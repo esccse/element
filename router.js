5fa8700a2fbc1eac9d9ca3daae7213cd26d5a9f2
esccse <670583006@qq.com>
2020-01-14 19:27:35 +0800

    Update router.js

--- router.js
+++ router.js
@@ -7,42 +7,58 @@ const ddd = () => import('@/components-client/ddd');
 Vue.use(Router);
 
 const routes = [
-    {
-        path: '/aaa',
-        name: 'aaa',
-        component: aaa,
-        meta: {
-            requireAuth: false,
-            requireMenuAuth: false
-        }
+   {
+        path: '',
+        redirect: '/home1'
     },
     {
-        path: '/bbb',
-        name: 'bbb',
-        component: bbb,
-        meta: {
-            requireAuth: false,
-            requireMenuAuth: false
-        }
+        path: '/home1',
+        name: 'home1',
+        component: Home1,
+        children: [
+            {
+                path: 'aaa',
+                name: 'aaa',
+                component: aaa,
+                meta: {
+                    requireAuth: false,
+                    requireMenuAuth: false
+                }
+            },
+            {
+                path: 'bbb',
+                name: 'bbb',
+                component: bbb,
+                meta: {
+                    requireAuth: false,
+                    requireMenuAuth: false
+                }
+            },
+            {
+                path: 'ccc',
+                name: 'ccc',
+                component: ccc,
+                meta: {
+                    requireAuth: false,
+                    requireMenuAuth: false
+                }
+            },
+            {
+                path: 'ddd',
+                name: 'ddd',
+                component: ddd,
+                meta: {
+                    requireAuth: false,
+                    requireMenuAuth: false
+                }
+            },
+        ]
     },
     {
-        path: '/ccc',
-        name: 'ccc',
-        component: ccc,
-        meta: {
-            requireAuth: false,
-            requireMenuAuth: false
-        }
-    },
-    {
-        path: '/ddd',
-        name: 'ddd',
-        component: ddd,
-        meta: {
-            requireAuth: false,
-            requireMenuAuth: false
-        }
-    },
+        path: '/home2',
+        name: 'home2',
+        component: Home2,
+    }
 ]
 
 export default new Router({
